bd3922fb3e1431b3ed0674c43e9e087bf88ad26b
wibi <iman.wibi@gmail.com>
2020-01-24 12:53:42 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = "192.168.33.11:3306";
+$db_user = "devopscilsy";
+$db_pass = "P@ssw0rd";
+$db_name = "dbsosmed";
+
+try {    
+    //create PDO connection 
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
+}
